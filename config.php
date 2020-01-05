83e358ab1e724e32550b91aa28ad0889ae1a83b3
nikhil.b-at-011229092692 <nikhilbabychan07@gmail.com>
2020-01-06 00:11:59 +0530

    Uplugin added

---
+++ config.php
@@ -0,0 +1,14 @@
+<?php
+/*-----------------------------------------------------------------
+	TRANSPICIOUS CONFIGURATION FILE
+	check out https://github.com/Niyko/Transpicious#configuring-transpicious
+------------------------------------------------------------------*/
+
+//Language which is used to write the content of the website 
+$_DEFAULT_CONVERT_FROM = "en";
+
+//defualt language of the website, that will be shown to the user
+$_DEFAULT_CONVERT_TO = "en";
+
+
+?>
